00b9ed96f19bcea6d1fb1c18f3ee17fccad5efee
Vazgen Soghoyan <sogvazulchik@gmail.com>
2023-11-09 17:04:02 +0500

    starteqsdoing crookedlytherearemistakesneedinsertm

---
+++ MathProject/SystemOfLinearEqs.cs
@@ -0,0 +1,37 @@
+﻿namespace MathProject
+{
+    public class SystemOfLinearEqs
+    {
+        private Matrix A;
+        private Matrix B;
+
+        private int M;
+
+        public SystemOfLinearEqs(Matrix A, Matrix B)
+        {
+            if (!(B.M == B.M && B.M == A.N && B.N == 1)) throw new Exception();
+
+            this.A = A;
+            this.B = B;
+
+            this.M = A.M;
+        }
+
+        public Matrix GetA() => A;
+        public Matrix GetB() => B;
+
+        public Matrix GetSolutions()
+        {
+            var answers = new Complex[1, M];
+
+            var det = A.Determinant();
+
+            for (int i = 0; i < M; i++)
+            {
+                    
+            }
+
+            return new Matrix(answers);
+        }
+    }
+}
