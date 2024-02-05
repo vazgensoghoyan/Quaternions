1dcb6767cd0540f749b427b2ac78bc868b086105
Vazgen Soghoyan <sogvazulchik@gmail.com>
2024-02-05 22:20:10 +0500

    small changes

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿using Object;
+
+
+var objFile = new ObjFile(@"C:\Users\Acer\Desktop\1.obj");
+var model = objFile.GetModel(0);
+model.RotateZ(Math.PI / 4);
+model.RotateY(Math.PI / 9);
+objFile.Clear();
+objFile.AddModel(model);
