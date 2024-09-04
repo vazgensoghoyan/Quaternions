b0afa6d504caabfc8761ff4ebdd1ae16ba385165
Vazgen Soghoyan <sogvazulchik@gmail.com>
2024-09-04 16:55:53 +0300

    git ignore

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,15 +1,9 @@
 ﻿using Object;
-using MyMath;
 
 
-/*var objFile = new ObjFile(@"C:\Users\Acer\Desktop\1.obj"); // создается объект класса для работы с файлом
+var objFile = new ObjFile(@"C:\Users\Acer\Desktop\tree\tree.obj"); // создается объект класса для работы с файлом
 var model = objFile.GetModel(0); // так как файл уже прочтён (конструктор вызывается при создании
 model.RotateX(Math.PI / 3);
+model.RotateY(Math.PI / 6);
 objFile.Clear();
-objFile.AddModel(model);*/
-
-var q = new Quaternion(1, 2, 4, 5);
-var w = new Quaternion(3, 4, -1, 0);
-
-Console.WriteLine(q + w);
-Console.WriteLine(q * w);
+objFile.AddModel(model);
